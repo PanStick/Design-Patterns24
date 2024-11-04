91ab78da0f6ee2bf572ddbd7aa6a29a4dbbd0987
PanStick <tymoteusz.blaszcz@student.uj.edu.pl>
2024-11-04 21:06:18 +0100

    Iterator imp

---
+++ Wzorce Projektowe/Iterator/Iterator.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wzorce_Projektowe.Iterator
+{
+
+    //class BinaryTree
+    //{
+    //    public List<TreeNode> Nodes { get; set; }
+    //    public BinaryTreeIterator<TreeNode> iterator;
+
+    //    public BinaryTree(List<TreeNode> nodes)
+    //    {
+    //        Nodes = nodes;
+    //        CreateIterator();
+
+    //    }
+    //    void CreateIterator() { iterator = new BinaryTreeIterator<TreeNode>(Nodes); }
+    //}
+    class TreeNode
+    {
+        public int Val { get; set; }
+        public TreeNode? Left { get; set; }
+        public TreeNode? Right { get; set; }
+
+        public TreeNode() { }
+        public TreeNode(int val) { Val = val; }
+    }
+
+    interface IIterator<T>
+    {
+        bool HasMore();
+        T GetNext();
+    }
+
+    class BinaryTreeIterator : IIterator<TreeNode>
+    {
+        readonly Queue<TreeNode> queue = new();
+        public BinaryTreeIterator(List<TreeNode> tree)
+        {
+            queue.Enqueue(tree.First());
+        }
+        public bool HasMore() { return queue.Count > 0; }
+        public TreeNode GetNext()
+        {
+            if (HasMore())
+            {
+                TreeNode temp = queue.Dequeue();
+                if (temp.Left != null)
+                    queue.Enqueue(temp.Left);
+                if (temp.Right != null)
+                    queue.Enqueue(temp.Right);
+                return temp;
+            }
+            else throw new InvalidOperationException();
+        }
+    }
+
+    public interface IBinaryTreeIterable
+    {
+        void CreateIterator();
+    }
+
+    class BinaryTree : IBinaryTreeIterable
+    {
+        List<TreeNode> _tree;
+        BinaryTreeIterator _iterator;
+
+        public BinaryTree(List<TreeNode> tree)
+        {
+            _tree = tree;
+            CreateIterator();
+        }
+
+        public void CreateIterator() { _iterator = new(_tree); }
+
+        public BinaryTreeIterator GetIterator() { return _iterator; }
+
+        public bool HasMore() { return _iterator.HasMore(); }
+        public TreeNode GetNext() { return _iterator.GetNext(); }
+    }
+
+    class BinaryTree2 : IBinaryTreeIterable
+    {
+        List<TreeNode> _tree;
+        BinaryTreeIterator<TreeNode> _iterator;
+
+        public BinaryTree2(List<TreeNode> tree)
+        {
+            _tree = tree;
+            CreateIterator();
+        }
+        public IEnumerator GetEnumerator() { return _iterator.GetEnumerator(); }
+        //public TreeNode GetNext()
+        //{
+        //    IEnumerator temp = _iterator.GetEnumerator();
+        //    temp.MoveNext();
+        //    return (TreeNode)temp.Current;
+        //}
+
+        public void CreateIterator() { _iterator = new(_tree); }
+
+    }
+
+    class BinaryTreeIterator<TreeNode> : IEnumerable<Iterator.TreeNode>
+    {
+        readonly List<Iterator.TreeNode> _tree;
+
+        public BinaryTreeIterator(List<Iterator.TreeNode> nodes)
+        {
+            _tree = nodes;
+        }
+        public IEnumerator<Iterator.TreeNode> GetEnumerator()
+        {
+            Queue<Iterator.TreeNode> bfs = new();
+            bfs.Enqueue(_tree[0]);
+            while (bfs.Count > 0)
+            {
+                Iterator.TreeNode node = bfs.Dequeue();
+                if (node.Left != null)
+                    bfs.Enqueue(node.Left);
+                if (node.Right != null)
+                    bfs.Enqueue(node.Right);
+                Console.WriteLine("About to return");
+                yield return node;
+                Console.WriteLine("Returned");
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
+
+
--- Wzorce Projektowe/Program.cs
+++ Wzorce Projektowe/Program.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Wzorce_Projektowe.Observer;
+using Wzorce_Projektowe.Iterator;
+using Wzorce_Projektowe.MediatorSpace;
 
 namespace Wzorce_Projektowe
 {
@@ -13,16 +15,42 @@ namespace Wzorce_Projektowe
     {
         static void Main(string[] args)
         {
-            WeatherData weather = new();
-            CurrentConditions current = new(weather);
-            StatisticsDisplay statistics = new(weather);
-            weather.Notify(new(weather.GetHumidity(), weather.GetTemperature(), weather.GetPressure()));
-            current.Display();
-            statistics.DisplayStats();
-            Task.Delay(1000).Wait();
-            weather.Notify(new(weather.GetHumidity(), weather.GetTemperature(), weather.GetPressure()));
-            current.Display();
-            statistics.DisplayStats();
+            //WeatherData weather = new();
+            //CurrentConditions current = new(weather);
+            //StatisticsDisplay statistics = new(weather);
+            //weather.Notify(new(weather.GetHumidity(), weather.GetTemperature(), weather.GetPressure()));
+            //current.Display();
+            //statistics.DisplayStats();
+            //Task.Delay(1000).Wait();
+            //weather.Notify(new(weather.GetHumidity(), weather.GetTemperature(), weather.GetPressure()));
+            //current.Display();
+            //statistics.DisplayStats();
+
+            List<TreeNode> nodes = [new(1), new(3), new(2)];
+            nodes[0].Left = nodes[2];
+            nodes[0].Right = nodes[1];
+            BinaryTree2 tree = new(nodes);
+            foreach (TreeNode i in tree)
+                Console.WriteLine(i.Val);
+            BinaryTree tree1 = new(nodes);
+            
+            
+
+
+
+            //OrderProcesser processer = new();
+            //OrderReceiver receiver = new();
+            //OrderVerifier verifier = new();
+            //OrderLogger logger = new OrderLogger();
+
+            //Mediator mediator = new(receiver, verifier, processer, logger);
+            //receiver.Mediator = mediator;
+            //verifier.Mediator = mediator;
+            //processer.Mediator = mediator;
+
+            //receiver.ReceiveOrder();
+            //verifier.VerifyOrder();
+            //processer.ProcessOrder();
 
         }
     }
