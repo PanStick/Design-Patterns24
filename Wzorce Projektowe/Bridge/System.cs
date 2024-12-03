95241fd3ba0351f15a08cf3a18d20b00b762cd36
tymbszcz <93001484+PanStick@users.noreply.github.com>
2024-12-03 08:04:55 +0100

    Bridge

---
+++ Wzorce Projektowe/Bridge/System.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wzorce_Projektowe.Bridge
+{
+    enum SupportedFileTypes
+    {
+        XML,
+        CSV,
+        FixedLength
+    }
+
+    abstract class AddressSystem(WriteImplementation imp)
+    {
+        protected WriteImplementation imp = imp;
+
+        public abstract void Write(SupportedFileTypes fileType);
+    }
+
+    class USAddress(WriteImplementation imp) : AddressSystem(imp)
+    {
+        public override void Write(SupportedFileTypes fileType)
+        {
+            imp.WriteUSAddress(fileType);
+        }
+    }
+
+    class UKAddress(WriteImplementation imp) : AddressSystem(imp)
+    {
+        public override void Write(SupportedFileTypes fileType)
+        {
+            imp.WriteUKAddress(fileType);
+        }
+    }
+
+    class PLAddress(WriteImplementation imp) : AddressSystem(imp)
+    {
+        public override void Write(SupportedFileTypes fileType)
+        {
+            imp.WritePlAddress(fileType);
+        }
+    }
+}
---
+++ Wzorce Projektowe/Bridge/WriteImplementation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wzorce_Projektowe.Bridge
+{
+    class WriteImplementation
+    {
+        public void WriteUSAddress(SupportedFileTypes fileType)
+        {
+            switch (fileType)
+            {
+                case SupportedFileTypes.XML: XMLFile.WriteUSAddress(); break;
+                case SupportedFileTypes.CSV: CSVFile.WriteUSAddress(); break;
+                case SupportedFileTypes.FixedLength: FixedLengthFile.WriteUSAddress(); break;
+            }
+        }
+
+        public void WriteUKAddress(SupportedFileTypes fileType)
+        {
+            switch (fileType)
+            {
+                case SupportedFileTypes.XML: XMLFile.WriteUKAddress(); break;
+                case SupportedFileTypes.CSV: CSVFile.WriteUKAddress(); break;
+                case SupportedFileTypes.FixedLength: FixedLengthFile.WriteUKAddress(); break;
+            }
+        }
+
+        public void WritePlAddress(SupportedFileTypes fileType)
+        {
+            switch (fileType)
+            {
+                case SupportedFileTypes.XML: XMLFile.WritePLAddress(); break;
+                case SupportedFileTypes.CSV: CSVFile.WritePLAddress(); break;
+                case SupportedFileTypes.FixedLength: FixedLengthFile.WritePLAddress(); break;
+            }
+        }
+
+    }
+
+    static class XMLFile
+    {
+        public static void WriteUSAddress() { }
+        public static void WriteUKAddress() { }
+        public static void WritePLAddress() { }
+    }
+
+    static class CSVFile
+    {
+        public static void WriteUSAddress() { }
+        public static void WriteUKAddress() { }
+        public static void WritePLAddress() { }
+    }
+
+    static class FixedLengthFile
+    {
+        public static void WriteUSAddress() { }
+        public static void WriteUKAddress() { }
+        public static void WritePLAddress() { }
+    }
+
+}
